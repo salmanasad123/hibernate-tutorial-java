b433c0a67a6efadde8e132d6e4c3396ddc00556a
salmanasad123 <slman.me@gmail.com>
2021-10-04 11:49:53 +0500

    Added hibernate config file and created Entity class to map our class to database

---
+++ src/com/luv2code/hibernate/demo/entity/Student.java
@@ -0,0 +1,78 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+// to map the class to database table we use @Entity annotation
+@Entity
+public class Student {
+
+    @javax.persistence.Id
+    @Column(name = "id")
+    private int Id;
+
+    @Column(name = "first_name")  // name of the column in the database
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    // no arg constructor
+    public Student() {
+
+    }
+
+    // we add constructor for convenience, because we will use it later on
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // adding toString method just for debugging purposes
+    @Override
+    public String toString() {
+        return "Student{" +
+                "Id=" + Id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
